395fd884f8144b103aa93ae2c56140627a629597
Nakul Srikanth <nakul_0702@yahoo.com>
2024-09-10 23:36:59 -0700

    Proj 1 Final Submission V2

--- 1/scripts.js
+++ 1/scripts.js
@@ -8,13 +8,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Image switching functionality
+// Image and info switching functionality
 const imageButtons = document.querySelectorAll('.image-buttons button');
 const mainImage = document.getElementById('main-image');
+const imageInfo = document.getElementById('image-info');
 
 imageButtons.forEach(button => {
     button.addEventListener('click', function() {
+        // Update the main image
         const imgUrl = this.getAttribute('data-img');
         mainImage.src = imgUrl;
+
+        // Update the image info
+        const name = this.getAttribute('data-name');
+        const g1 = this.getAttribute('data-g1');
+        const g2 = this.getAttribute('data-g2');
+        imageInfo.innerHTML = `<b>${name}</b> [Translational Vector for G channel: ${g1}] [Translational Vector for R channel: ${g2}]`;
     });
 });
